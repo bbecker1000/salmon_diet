73c242cc41551799122ca7e043ca53498256c59c
Ellington William Chen <ellington.chen@gmail.com>
2026-05-20 15:16:04 -0700

    adjusting lm for species comparison

--- Code/FCF_graphs.R
+++ Code/FCF_graphs.R
@@ -12,11 +12,25 @@ ggplot(DietData_env %>% filter(LifeStage == "YoY"), aes(x = ForkLength, y =FishW
   geom_smooth(method = "lm", se = FALSE) + 
   facet_wrap(~ SpeciesCode, scales = "free")
 
+ggplot(DietData_env %>% filter(LifeStage == "YoY"), aes(x = ForkLength, y =FishWeight, color= SpeciesCode)) +
+  geom_point(aes(color = SpeciesCode), size = 2, alpha = 0.4) +
+  geom_smooth(method = "lm", se = FALSE)
+
 #getting slopes (slope= approximate for FCF) ** all slopes are 0.351? 
 DietData_env %>% 
   group_by(SpeciesCode) %>%
   do(tidy(lm(FishWeight ~ ForkLength, data = DietData_env %>% filter(LifeStage== "YoY")))) %>% 
   select(SpeciesCode, term, estimate)
+
+#
+for (species in unique(DietData_env$SpeciesCode)) {
+  DietData_temp <- DietData_env %>%
+    filter(LifeStage == "YoY",
+           SpeciesCode == species)
+  print(species)
+  print(summary(lm(FishWeight ~ ForkLength, data = DietData_temp)))
+}
+summary(lm(FishWeight ~ ForkLength, data = DietData_env %>% filter(LifeStage== "YoY")))
 
 #pulling info from our data sets to compare to literature lmao 
 #FCF for CH: 1.000656, CO: 1.106940, SH: 1.101246
