da57c028481567b31c303e511a34be04d14dc67d
Ellington William Chen <ellington.chen@gmail.com>
2026-06-03 09:44:49 -1000

    Salmon survey data frames

--- Code/2.1.Salmon_Survey_Data.R
+++ Code/2.1.Salmon_Survey_Data.R
@@ -1,6 +1,6 @@
-# last updated: 05/28/26
-# 
-
+# last updated: 06/03/26
+# creating salmon snorkel survey and seining + electrofishing measurement survey data for general discussion
+# revised syntax for certain variables, filtered dates, location, and species life stage to within study range and selected relevant variables
 
 # Libraries -----------------------------------------------------------------
 
@@ -21,7 +21,7 @@ fish_snorkel <- snorkel_survey_data %>%
          LifeStage = case_match(LifeStage,
                                 "yoy" ~ "YoY",
                                 .default = LifeStage)) %>%
-  # limit to years gut content data set
+  # limit to years in gut content data set
   # study system is only redwood creek, exclude fern creek
   # limit survey dates to within the gut contents data set
   filter(FieldSeason %in% c(2020, 2022), 
@@ -37,12 +37,29 @@ fish_snorkel <- snorkel_survey_data %>%
          Watershed, StreamName, HabitatDescriptionSnorkel, 
          Pass, Visibility, TimeElapsed_s)
 
-
 fish_measurement <- measurement_survey_data %>% 
+  # pull stream number from location code
   mutate(Date = as.Date(StartDate),
          StreamNumber = sub("RW-01-", "", LocationCode)) %>%
+  # limit to years in gut content data set
+  # study system is only redwood creek, exclude fern creek
+  # limit survey dates to within the gut contents data set
+  # limit life stage to within the gut contents data set
   filter(FieldSeason %in% c(2020, 2022),
          Watershed == "Redwood Creek",
          StreamName != "Fern Creek",
          month(Date) == 7 | month(Date) == 8,
-         LifeStage %in% c("YoY", "1+"))
+         LifeStage %in% c("YoY", "1+")) %>%
+  # salmon and count data
+  # location data
+  # habitat data
+  # morphometric data
+  select(SpeciesCode, LifeStage, NumberOfFish,
+         ID, EventID, LocationID, StartDate, FieldSeason, Latitude, Longitude, StreamNumber,
+         Watershed, StreamName,
+         ForkLength_mm, FishWeight_g)
+
+# Export ------------------------------------------------------------------
+
+write.csv(fish_snorkel,"Data/Created_Data/Fish_Snorkel.csv", row.names = FALSE)
+write.csv(fish_measurement, "Data/Created_Data/Fish_Measurement.csv", row.names = FALSE)
